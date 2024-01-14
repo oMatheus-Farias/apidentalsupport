0b20226df5fcaeb8c3f9c0c01ed5678d910452c6
unknown <matheusfariasdasneves@gmail.com>
2024-01-14 14:14:40 -0300

    feat: Adicionar novos campos de dados no schema do prisma

---
+++ prisma/migrations/20240114171129_create_tables_service/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "services" ADD COLUMN     "address" TEXT,
+ADD COLUMN     "contactClinic" TEXT,
+ADD COLUMN     "contactCustomer" TEXT,
+ADD COLUMN     "customer" TEXT,
+ADD COLUMN     "nameClinic" TEXT,
+ADD COLUMN     "nameProduct" TEXT;
